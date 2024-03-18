7e35c2b267bedc565ecbfb1854532b3d650dc906
Er-pratibha <pratibhaanuguliamncs@gmail.com>
2024-03-18 13:29:32 +0530

    Content added

--- src/Data/Cement.js
+++ src/Data/Cement.js
@@ -1,10 +1,43 @@
 export const UltratechDat=[
     {
-        "id": "",     
-        "image": "",     
-        "productDic": "",     
-        "pice": "",     
-        "Wight": "",     
+        "id": "1",     
+        "image": "./assets/Ultra/33-grade-cement.webp",
+        "Grade": "33 Grade Cement",     
+        "productDesc": "33 grade cement is commonly used for general construction work and plastering.has a compressive strength of 33 N/mm² after 28 days of curing. This type of cement is suitable for use in construction projects where the requirement for high strength is not critical. It is not suitable for concrete mix above M20.",     
+        "price": "Rs. 280.00",     
+        "Weight": "320kg",     
+        "brand": "Ultra Tech",
+    },
+    
+    {
+        "id": "2",     
+        "image": "",
+        "Grade": "",     
+        "productDesc": "",     
+        "price": "",     
+        "Weight": "",     
         "brand": "",
+    },
+
+    {
+        "id": "3",     
+        "image": "", 
+        "Grade": "",    
+        "productDesc": "",     
+        "price": "",     
+        "Weight": "",     
+        "brand": "",
+    },
+]
+
+export const ACCData=[
+    {
+        "id": "3",     
+        "image": "",
+        "Grade": "",     
+        "productDesc": "",     
+        "price": "",     
+        "Weight": "",     
+        "brand": "", 
     },
 ]
