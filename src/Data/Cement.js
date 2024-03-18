8188c186b4c0365bec961865099547e76c4c93f9
Aravireddy <raviannapureddy06@gmail.com>
2024-03-18 12:47:02 +0530

    all data add

---
+++ src/Data/Cement.js
@@ -0,0 +1,10 @@
+export const UltratechDat=[
+    {
+        "id": "",     
+        "image": "",     
+        "productDic": "",     
+        "pice": "",     
+        "Wight": "",     
+        "brand": "",
+    },
+]
