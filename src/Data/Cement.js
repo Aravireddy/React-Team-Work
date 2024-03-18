35c9f64c9cd8bc4f067b686242885f8b8b7aa171
Er-pratibha <pratibhaanuguliamncs@gmail.com>
2024-03-18 17:18:06 +0530

    Cement details updated

--- src/Data/Cement.js
+++ src/Data/Cement.js
@@ -4,7 +4,7 @@ export const UltratechDat=[
         "image": "./assets/Ultra/33-grade-cement.webp",
         "Grade": "33 Grade Cement",     
         "productDesc": "33 grade cement is commonly used for general construction work and plastering.has a compressive strength of 33 N/mm² after 28 days of curing. This type of cement is suitable for use in construction projects where the requirement for high strength is not critical. It is not suitable for concrete mix above M20.",     
-        "price": "Rs. 280.00",     
+        "price": "Rs. 280/-",     
         "Weight": "320kg",     
         "brand": "Ultra Tech",
     },
@@ -12,32 +12,181 @@ export const UltratechDat=[
     {
         "id": "2",     
         "image": "",
-        "Grade": "",     
-        "productDesc": "",     
-        "price": "",     
-        "Weight": "",     
-        "brand": "",
+        "Grade": "43 Grade cement",     
+        "productDesc": "43 grade cement is the most commonly used grade of cement in India. It has a compressive strength of 43 N/mm² after 28 days of curing. It's suitable for use in construction projects where moderate to high strength is required, such as plain concrete or plastering works. It is also used to make precast items such as tiles, blocks, pipes, etc. It has a higher compressive strength than 33-grade cement and is suitable for medium-scale construction projects. It is suitable for concrete mix up to M30.",     
+        "price": "Rs.380/-",     
+        "Weight": "340kg",     
+        "brand": "Ultra Tech",
     },
 
     {
         "id": "3",     
         "image": "", 
-        "Grade": "",    
-        "productDesc": "",     
-        "price": "",     
-        "Weight": "",     
-        "brand": "",
+        "Grade": "53 Grade Cement",    
+        "productDesc": "53 grade cement is the highest grade of cement available in India. It has a compressive strength of 53 N/mm² after 28 days of curing. This type of cement is suitable for use in construction projects where high strength is required, such as in the construction of high-rise buildings, dams, and heavy-duty industrial structures. It has a higher compressive strength than both 33 and 43-grade cement, making it more durable and long-lasting. Suitable for concrete mix above M25",     
+        "price": "Rs.600/-",     
+        "Weight": "340kg",     
+        "brand": "Ultra Tech",
     },
 ]
 
 export const ACCData=[
     {
-        "id": "3",     
+        "id": "4",     
         "image": "",
-        "Grade": "",     
-        "productDesc": "",     
-        "price": "",     
-        "Weight": "",     
-        "brand": "", 
+        "Grade": "43 Grade Cement (OPC 43 Grade)",     
+        "productDesc": "ACC Cement is the most used cement in all constructions including plain and reinforced cement concrete, brick and stone masonry, floors and plastering. It is also used in the finishing of all types of buildings, bridges, culverts, roads, water retaining structures, etc.",     
+        "price": "Rs.380/-",     
+        "Weight": "50kg",     
+        "brand": "ACC", 
+    },
+
+    {
+        "id": "5",     
+        "image": "",
+        "Grade": "53 Grade Cement",     
+        "productDesc": "This is an Ordinary Portland Cement which surpasses the requirements of IS: 12269-53 Grade. It is produced from high quality clinker ground with high purity gypsum.ACC 53 Grade OPC provides high strength and durability to structures because of its optimum particle size distribution, superior crystalline structure and balanced phase composition.",     
+        "price": "Rs.410/-",     
+        "Weight": "50kg",     
+        "brand": "ACC", 
+    },
+]
+
+export const AmbujaData=[
+    {
+        "id": "6",     
+        "image": "",
+        "Grade": "33 Grade",     
+        "productDesc": "Compressive Strength: Achieves 33 N/mm² after 28 days of construction. Usage: Suitable for basic applications such as making walls, floors, and plaster.",     
+        "price": "Rs. 280/-",     
+        "Weight": "50kg",     
+        "brand": "Ambuja Cement", 
+    },
+
+    {
+        "id": "7",     
+        "image": "",
+        "Grade": "43 Grade",     
+        "productDesc": "Compressive Strength: Attains 43 N/mm² after 28 days. Application: Ideal for general construction purposes, including structural elements like columns, beams, and slabs",
+        "price": "Rs. 340/-",     
+        "Weight": "50kg",     
+        "brand": "Ambuja Cement", 
+    },
+
+    {
+        "id": "8",     
+        "image": "",
+        "Grade": "53 Grade",     
+        "productDesc": "Compressive Strength: Reaches 53 N/mm² after 28 days.Significance: Ambuja Cement was the first to introduce 53-grade cement in the Indian market. Quality Certification: Ambuja became the first cement company to receive the ISO 9002 quality certification.Raw Material: Approximately 25% of the company’s production of high-strength Portland Pozzolana Cement (PPC) uses fly ash (a waste produced in thermal power plants) as raw material.",
+        "price": "Rs. 340/-",     
+        "Weight": "50kg",     
+        "brand": "Ambuja Cement", 
+    }, 
+]
+
+export const JaypeeData=
+[
+    {
+        "id": "9",     
+        "image": "",
+        "Grade": "33 Grade",     
+        "productDesc": "Compressive Strength: Achieves 33 N/mm² after 28 days of construction. Usage: Suitable for basic applications such as making walls, floors, and plaster.",     
+        "price": "Rs. 280/-",     
+        "Weight": "50kg",     
+        "brand": "Jaypee Cement", 
+    },
+
+    {
+        "id": "10",     
+        "image": "",
+        "Grade": "43 Grade",     
+        "productDesc": "Compressive Strength: Attains 43 N/mm² after 28 days. Application: Ideal for general construction purposes, including structural elements like columns, beams, and slabs",
+        "price": "Rs. 340/-",     
+        "Weight": "50kg",     
+        "brand": "Jaypee Cement", 
+    },
+
+    {
+        "id": "11",     
+        "image": "",
+        "Grade": "53 Grade",     
+        "productDesc": "Compressive Strength: Reaches 53 N/mm² after 28 days.Significance: Jaypee Cement (OPC-53 Grade) surpasses the properties of OPC 53 grade as defined in IS: 12269 – 1987.Quality Certification: Jaypee Rewa Plant Quality control laboratory is accredited by the National Accreditation Board for Calibration & Testing Laboratories for chemical and mechanical cement testing.Raw Material: Jaypee Cement (OPC-53 Grade) is produced from enriched limestone, resulting in high-quality clinker.",
+        "price": "Rs. 340/-",     
+        "Weight": "50kg",     
+        "brand": "Jaypee Cement", 
+    }, 
+
+]
+
+export const DalmiaData=[
+    {
+        "id": "12",     
+        "image": "",
+        "Grade": "33 Grade",     
+        "productDesc": "Compressive Strength: Dalmia PPC produces less heat of hydration and offers greater resistance to the attack of sulfate, chloride, and silica reactions.Application: Ideal for general construction and work exposed to aggressive weather conditions.",     
+        "price": "Rs. 280/-",     
+        "Weight": "50kg",     
+        "brand": "Dalmia Cement", 
+    },
+
+    {
+        "id": "13",     
+        "image": "",
+        "Grade": "Not Specified, but available in 43 and 53 grades",     
+        "productDesc": "Properties: i. Lower heat of hydration. ii. Improved resistance to chemical attacks.iii. Reduced tendency to crack. c. Ideal for: All kinds of construction exposed to marine environments.",
+        "price": "Rs. 340/-",     
+        "Weight": "50kg",     
+        "brand": "Dalmia Cement", 
+    },
+
+    {
+        "id": "14",     
+        "image": "",
+        "Grade": "Not specified, but it is commonly available in 43 and 53 ",     
+        "productDesc": "i. Optimum blend of high-reactive silica and slag.ii. Better workability.iii. Higher long-term strength gain",
+        "price": "Rs. 380/-",     
+        "Weight": "50kg",     
+        "brand": "Dalmia Cement", 
+    }, 
+    {
+        "id": "15",     
+        "image": "",
+        "Grade": "Not specified, but it is commonly available in 43 and 53 ",     
+        "productDesc": "Features: i. Quicker initial setting time.ii. Higher early strength. c. Ideal for: High-strength concrete applications in high-speed and  mechanized construction.5) Remember that the choice of cement grade depends on your specific  construction requirements. Dalmia Cement offers a range of options to meet  various needs, from general construction to specialized applications.",
+        "price": "Rs. 380/-",     
+        "Weight": "50kg",     
+        "brand": "Dalmia Cement", 
+    }, 
+]
+export const BirlaCementData=[
+    {
+        "id": "16",     
+        "image": "",
+        "Grade": " Birla.A1 Premium Cement – OPC 53 Grade:",     
+        "productDesc": "One of the pioneers of 53-Grade Cement in India.Launched as Orient Gold 53-Grade Cement in 1992.Known for strength and durability.Suitable for all types of industrial, residential, and commercial constructions.Certified for QMS ISO 9001:2015, EMS ISO 14001:2015, OHSMS 18001:2015, ISO 19011:2018, ISO 50001:2011, and ISO 27001:2013.Awarded the prestigious TPM (Total Plant Maintenance) excellence award by the Japan Institute of Plant Maintenance.Received numerous awards for green and safety practices.",
+        "price": "Rs. 380/-",     
+        "Weight": "50kg",     
+        "brand": "Biela Cements", 
+        
+    },
+    {
+        "id": "17",     
+        "image": "",
+        "Grade": "Birla Super 53 Grade Cement",     
+        "productDesc": "High strength.Anti-corrosive properties.Green Pro certified.Suitable for construction of dams, roads, runways, RCC, and prestressed concrete of higher grades",
+        "price": "Rs. 420/-",     
+        "Weight": "50kg",     
+        "brand": "Biela Cements", 
+        
+    },
+    {
+        "id": "18",     
+        "image": "",
+        "Grade": "Birla Super 43 Grade Cement",     
+        "productDesc": "High strength.Anti-corrosive properties.Green Pro certified.Suitable for construction of dams, roads, runways, RCC, and prestressed concrete of higher grades",
+        "price": "Rs. 420/-",     
+        "Weight": "50kg",     
+        "brand": "Biela Cements", 
+        
     },
 ]
